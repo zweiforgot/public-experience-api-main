eecac0ffa3ad27f2fca8d3ef298e77140e934a6b
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-10-08 22:31:14 -0400

    fix: CORS issue

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -3,6 +3,14 @@ import { swaggerUI } from '@hono/swagger-ui';
 
 const app = new OpenAPIHono();
 
+app.use('*', (ctx, next) => {
+    ctx.header('Access-Control-Allow-Origin', '*');
+    ctx.header('Access-Control-Allow-Methods', 'GET');
+    ctx.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    return next();
+});
+
 app.get('/', swaggerUI({ url: '/docs/json' }));
 
 app.doc('/docs/json', {

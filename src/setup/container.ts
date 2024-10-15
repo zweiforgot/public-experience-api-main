faaebaee278083e1fd23bc81f2a218262268dd65
LuckFire <39076891+LuckFire@users.noreply.github.com>
2024-10-15 16:55:09 -0400

    chore: remove node-cache

--- src/setup/container.ts
+++ src/setup/container.ts
@@ -1,10 +1,8 @@
-import NodeCache from "node-cache";
 import database from "@/lib/database";
 import redis_cache from "@/lib/redis-cache";
 
 const container = {
     database,
-    cache: new NodeCache(),
     redis: redis_cache
 };
 

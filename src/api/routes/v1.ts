bb6a66b5c023aea1e9ff9ce99d0937f17fe7a76d
ZweiForgot <zwei.lmao.mail@gmail.com>
2024-10-15 17:53:31 -0400

    yuh

--- src/api/routes/v1.ts
+++ src/api/routes/v1.ts
@@ -4,6 +4,6 @@ import fracturedfranchise from "@/api/routes/fracturedfranchise";
 const v1 = new OpenAPIHono();
 
 v1.get('/', (res) => res.json({ status: "OK", endpoint: res.req.path }));
-v1.route('/oaklands', fracturedfranchise);
+v1.route('/fracturedfranchise', fracturedfranchise);
 
 export default v1;
--- src/lib/schemas/FracturedFranchise/Characters.ts
+++ src/lib/schemas/FracturedFranchise/Characters.ts
@@ -1,38 +1,40 @@
 import { z } from '@hono/zod-openapi';
 
-const Characters = z.record(z.object({
-    ID: z.string(),
-    Name: z.string(),
-    Description: z.nullable(z.string()),
-    AccentColor: z.string(),
-    Category: z.string(),
-    Restriction: z.nullable(z.string()),
-    PriceInfo: z.any(),
-    Media: z.object({
-        Headshot: z.string(),
-        Fullbody: z.string(),
-        Bust: z.string()
-    }),
-    Animations: z.record(z.string(), z.number()),
-    ActionInfo: z.record(z.string(), z.record(z.number(), z.any())),
-    PhysicalMappings: z.object({
-        Head: z.nullable(z.string()),
-        LowerTorso: z.nullable(z.string()),
-        LeftHand: z.nullable(z.string()),
-        RightHand: z.nullable(z.string())
-    }),
-    MovementInfo: z.object({
-        WalkSpeed: z.number(),
-        RunInfo: z.object({
-            CanRun: z.boolean(),
-            RunSpeed: z.number()
+const Characters = z.object({
+    characters: z.record(z.object({
+        ID: z.string(),
+        Name: z.string(),
+        Description: z.nullable(z.string()),
+        AccentColor: z.string(),
+        Category: z.string(),
+        Restriction: z.nullable(z.string()),
+        PriceInfo: z.any(),
+        Media: z.object({
+            Headshot: z.string(),
+            Fullbody: z.string(),
+            Bust: z.string()
         }),
-        CrawlInfo: z.object({
-            CanCrawl: z.boolean(),
-            CrawlSpeed: z.number()
+        Animations: z.record(z.string(), z.number()),
+        ActionInfo: z.record(z.string(), z.record(z.number(), z.any())),
+        PhysicalMappings: z.object({
+            Head: z.nullable(z.string()),
+            LowerTorso: z.nullable(z.string()),
+            LeftHand: z.nullable(z.string()),
+            RightHand: z.nullable(z.string())
+        }),
+        MovementInfo: z.object({
+            WalkSpeed: z.number(),
+            RunInfo: z.object({
+                CanRun: z.boolean(),
+                RunSpeed: z.number()
+            }),
+            CrawlInfo: z.object({
+                CanCrawl: z.boolean(),
+                CrawlSpeed: z.number()
+            })
         })
-    })
-}));
+    }))
+});
 
 export type CharactersSchema = z.infer<typeof Characters>;
 
